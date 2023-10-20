ae2ffca38395f55ee46ac4ef08d8ce0c90cf5914
esem <stratos.marin@gmail.com>
2023-10-21 00:28:54 +0300

    throw out the demos from main.py

---
+++ app/demos/product.py
@@ -0,0 +1,9 @@
+from app.backend.session import create_session
+from app.services.product import ProductService
+import logging
+
+def run():
+    with create_session() as session:
+        product_service = ProductService(session)
+        fetched_product = product_service.get_product(1)  # let's assume product_id 1 exists
+        logging.info(f"Retrieved product: {fetched_product}")
