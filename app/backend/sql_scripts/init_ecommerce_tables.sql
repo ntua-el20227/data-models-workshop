578667a06bcaf2005fb6f5b193b061885de67308
esem <stratos.marin@gmail.com>
2023-10-20 22:39:41 +0300

    group the statements, add resets

--- app/backend/sql_scripts/init_ecommerce_tables.sql
+++ app/backend/sql_scripts/init_ecommerce_tables.sql
@@ -15,16 +15,8 @@ CREATE TABLE IF NOT EXISTS ecommerce_schema.users (
     password VARCHAR NOT NULL,
     email VARCHAR NOT NULL UNIQUE,
     registration_date DATE NOT NULL,
-    user_type TEXT CHECK (user_type IN ('admin', 'customer')) -- Assuming two types: admin and customer
+    user_type TEXT CHECK (user_type IN ('admin', 'customer'))
 );
-
--- Inserting data into ecommerce_schema.users
-DELETE FROM ecommerce_schema.users;
-INSERT INTO ecommerce_schema.users (username, password, email, registration_date, user_type) VALUES
-('john_doe', 'password123', 'john.doe@example.com', '2023-10-19', 'customer'),
-('alice_smith', 'securepass', 'alice.smith@example.com', '2023-09-15', 'admin');
-
----------------------------------------------------------------------------------------------------------
 
 -- Supplier table
 CREATE TABLE IF NOT EXISTS ecommerce_schema.supplier (
@@ -32,14 +24,6 @@ CREATE TABLE IF NOT EXISTS ecommerce_schema.supplier (
     name VARCHAR NOT NULL,
     contact_details VARCHAR
 );
-
--- Inserting data into ecommerce_schema.supplier
-DELETE FROM ecommerce_schema.supplier;
-INSERT INTO ecommerce_schema.supplier (name, contact_details) VALUES
-('TechSupplier Inc.', '1234 Tech St, Silicon Valley'),
-('StyleHouse', '5678 Fashion Ave, New York');
-
----------------------------------------------------------------------------------------------------------
 
 -- Product table
 CREATE TABLE IF NOT EXISTS ecommerce_schema.product (
@@ -49,19 +33,11 @@ CREATE TABLE IF NOT EXISTS ecommerce_schema.product (
     price FLOAT NOT NULL,
     stock_count INT NOT NULL,
     category VARCHAR,
-    parent_product_id INT REFERENCES ecommerce_schema.product(product_id) -- Self-referencing for parent-child products
+    parent_product_id INT REFERENCES ecommerce_schema.product(product_id),
+    supplier_id INT REFERENCES ecommerce_schema.supplier(supplier_id)
 );
 
--- Adding a relationship between Product and Supplier
-ALTER TABLE ecommerce_schema.product ADD COLUMN supplier_id INT REFERENCES ecommerce_schema.supplier(supplier_id);
 
--- Inserting data into ecommerce_schema.product
-DELETE FROM ecommerce_schema.product;
-INSERT INTO ecommerce_schema.product (name, description, price, stock_count, category, parent_product_id, supplier_id) VALUES
-('Laptop', 'Dell XPS 13', 1000.50, 100, 'Electronics', NULL, 1),
-('Fashion Hat', 'Stylish summer hat', 25.00, 200, 'Fashion', NULL, 2);
-
----------------------------------------------------------------------------------------------------------
 
 -- Orders table
 CREATE TABLE IF NOT EXISTS ecommerce_schema.orders (
@@ -69,66 +45,34 @@ CREATE TABLE IF NOT EXISTS ecommerce_schema.orders (
     user_id INT REFERENCES ecommerce_schema.users(user_id),
     order_date DATE NOT NULL,
     shipping_date DATE,
-    status TEXT CHECK (status IN ('pending', 'shipped', 'delivered', 'cancelled')) -- Assuming some typical order statuses
+    status TEXT CHECK (status IN ('pending', 'shipped', 'delivered', 'cancelled'))
 );
-
--- Inserting data into ecommerce_schema.orders
-DELETE FROM ecommerce_schema.orders;
-INSERT INTO ecommerce_schema.orders (user_id, order_date, shipping_date, status) VALUES
-(1, '2023-10-10', '2023-10-12', 'shipped'),
-(2, '2023-09-20', '2023-09-23', 'delivered');
-
----------------------------------------------------------------------------------------------------------
 
 -- OrderDetails table
 CREATE TABLE IF NOT EXISTS ecommerce_schema.orderdetails (
     order_id INT REFERENCES ecommerce_schema.orders(order_id),
     product_id INT REFERENCES ecommerce_schema.product(product_id),
     quantity INT NOT NULL,
-    PRIMARY KEY (order_id, product_id) -- Composite primary key
+    PRIMARY KEY (order_id, product_id)
 );
-
--- Inserting data into ecommerce_schema.orderdetails
-DELETE FROM ecommerce_schema.orderdetails;
-INSERT INTO ecommerce_schema.orderdetails (order_id, product_id, quantity) VALUES
-(1, 1, 2),
-(2, 2, 3);
-
----------------------------------------------------------------------------------------------------------
 
 -- Payment table
 CREATE TABLE IF NOT EXISTS ecommerce_schema.payment (
     payment_id SERIAL PRIMARY KEY,
     order_id INT REFERENCES ecommerce_schema.orders(order_id),
-    payment_method TEXT CHECK (payment_method IN ('credit_card', 'paypal', 'bank_transfer')), -- Assuming three payment methods
+    payment_method TEXT CHECK (payment_method IN ('credit_card', 'paypal', 'bank_transfer')),
     payment_date DATE NOT NULL,
     amount FLOAT NOT NULL
 );
-
--- Inserting data into ecommerce_schema.payment
-DELETE FROM ecommerce_schema.payment;
-INSERT INTO ecommerce_schema.payment (order_id, payment_method, payment_date, amount) VALUES
-(1, 'credit_card', '2023-10-10', 2001.00),
-(2, 'paypal', '2023-09-20', 75.00);
-
----------------------------------------------------------------------------------------------------------
 
 -- Review table
 CREATE TABLE IF NOT EXISTS ecommerce_schema.review (
     review_id SERIAL PRIMARY KEY,
     product_id INT REFERENCES ecommerce_schema.product(product_id),
     user_id INT REFERENCES ecommerce_schema.users(user_id),
-    rating INT CHECK (rating BETWEEN 1 AND 5), -- Assuming a rating scale from 1 to 5
+    rating INT CHECK (rating BETWEEN 1 AND 5),
     review_text VARCHAR
 );
-
--- Inserting data into ecommerce_schema.review
-DELETE FROM ecommerce_schema.review;
-INSERT INTO ecommerce_schema.review (product_id, user_id, rating, review_text) VALUES
-(1, 1, 4, 'Good laptop but a bit expensive.'),
-(2, 2, 5, 'Love this hat!');
-
----------------------------------------------------------------------------------------------------------
 
 -- Wishlist table
 CREATE TABLE IF NOT EXISTS ecommerce_schema.wishlist (
@@ -136,13 +80,6 @@ CREATE TABLE IF NOT EXISTS ecommerce_schema.wishlist (
     user_id INT REFERENCES ecommerce_schema.users(user_id),
     product_id INT REFERENCES ecommerce_schema.product(product_id)
 );
-
--- Inserting data into ecommerce_schema.wishlist
-DELETE FROM ecommerce_schema.wishlist;
-INSERT INTO ecommerce_schema.wishlist (user_id, product_id) VALUES
-(1, 2);
-
----------------------------------------------------------------------------------------------------------
 
 -- Shipment table
 CREATE TABLE IF NOT EXISTS ecommerce_schema.shipment (
@@ -153,8 +90,100 @@ CREATE TABLE IF NOT EXISTS ecommerce_schema.shipment (
     estimated_arrival DATE
 );
 
--- Inserting data into ecommerce_schema.shipment
+---------------------------------------------------------------------------------------------------------
+
+-- Deleting data in reverse order of dependency
 DELETE FROM ecommerce_schema.shipment;
+DELETE FROM ecommerce_schema.wishlist;
+DELETE FROM ecommerce_schema.review;
+DELETE FROM ecommerce_schema.payment;
+DELETE FROM ecommerce_schema.orderdetails;
+DELETE FROM ecommerce_schema.orders;
+DELETE FROM ecommerce_schema.product;
+DELETE FROM ecommerce_schema.supplier;
+DELETE FROM ecommerce_schema.users;
+
+---------------------------------------------------------------------------------------------------------
+
+-- Resetting the sequence of the serial primary keys
+
+-- Reset sequences for tables with SERIAL primary keys
+DO $$ 
+BEGIN
+    -- Resetting for users table
+    IF EXISTS (SELECT 1 FROM information_schema.sequences WHERE sequence_schema = 'ecommerce_schema' AND sequence_name = 'users_user_id_seq') THEN
+        ALTER SEQUENCE ecommerce_schema.users_user_id_seq RESTART WITH 1;
+    END IF;
+    
+    -- Resetting for supplier table
+    IF EXISTS (SELECT 1 FROM information_schema.sequences WHERE sequence_schema = 'ecommerce_schema' AND sequence_name = 'supplier_supplier_id_seq') THEN
+        ALTER SEQUENCE ecommerce_schema.supplier_supplier_id_seq RESTART WITH 1;
+    END IF;
+    
+    -- Resetting for product table
+    IF EXISTS (SELECT 1 FROM information_schema.sequences WHERE sequence_schema = 'ecommerce_schema' AND sequence_name = 'product_product_id_seq') THEN
+        ALTER SEQUENCE ecommerce_schema.product_product_id_seq RESTART WITH 1;
+    END IF;
+
+    -- Resetting for orders table
+    IF EXISTS (SELECT 1 FROM information_schema.sequences WHERE sequence_schema = 'ecommerce_schema' AND sequence_name = 'orders_order_id_seq') THEN
+        ALTER SEQUENCE ecommerce_schema.orders_order_id_seq RESTART WITH 1;
+    END IF;
+
+    -- Resetting for payment table
+    IF EXISTS (SELECT 1 FROM information_schema.sequences WHERE sequence_schema = 'ecommerce_schema' AND sequence_name = 'payment_payment_id_seq') THEN
+        ALTER SEQUENCE ecommerce_schema.payment_payment_id_seq RESTART WITH 1;
+    END IF;
+
+    -- Resetting for review table
+    IF EXISTS (SELECT 1 FROM information_schema.sequences WHERE sequence_schema = 'ecommerce_schema' AND sequence_name = 'review_review_id_seq') THEN
+        ALTER SEQUENCE ecommerce_schema.review_review_id_seq RESTART WITH 1;
+    END IF;
+
+    -- Resetting for wishlist table
+    IF EXISTS (SELECT 1 FROM information_schema.sequences WHERE sequence_schema = 'ecommerce_schema' AND sequence_name = 'wishlist_wishlist_id_seq') THEN
+        ALTER SEQUENCE ecommerce_schema.wishlist_wishlist_id_seq RESTART WITH 1;
+    END IF;
+
+    -- Resetting for shipment table
+    IF EXISTS (SELECT 1 FROM information_schema.sequences WHERE sequence_schema = 'ecommerce_schema' AND sequence_name = 'shipment_shipment_id_seq') THEN
+        ALTER SEQUENCE ecommerce_schema.shipment_shipment_id_seq RESTART WITH 1;
+    END IF;
+
+END $$;
+
+-- Inserting data in the order of dependency
+INSERT INTO ecommerce_schema.users (username, password, email, registration_date, user_type) VALUES
+('john_doe', 'password123', 'john.doe@example.com', '2023-10-19', 'customer'),
+('alice_smith', 'securepass', 'alice.smith@example.com', '2023-09-15', 'admin');
+
+INSERT INTO ecommerce_schema.supplier (name, contact_details) VALUES
+('TechSupplier Inc.', '1234 Tech St, Silicon Valley'),
+('StyleHouse', '5678 Fashion Ave, New York');
+
+INSERT INTO ecommerce_schema.product (name, description, price, stock_count, category, parent_product_id, supplier_id) VALUES
+('Laptop', 'Dell XPS 13', 1000.50, 100, 'Electronics', NULL, 1),
+('Fashion Hat', 'Stylish summer hat', 25.00, 200, 'Fashion', NULL, 2);
+
+INSERT INTO ecommerce_schema.orders (user_id, order_date, shipping_date, status) VALUES
+(1, '2023-10-10', '2023-10-12', 'shipped'),
+(2, '2023-09-20', '2023-09-23', 'delivered');
+
+INSERT INTO ecommerce_schema.orderdetails (order_id, product_id, quantity) VALUES
+(1, 1, 2),
+(2, 2, 3);
+
+INSERT INTO ecommerce_schema.payment (order_id, payment_method, payment_date, amount) VALUES
+(1, 'credit_card', '2023-10-10', 2001.00),
+(2, 'paypal', '2023-09-20', 75.00);
+
+INSERT INTO ecommerce_schema.review (product_id, user_id, rating, review_text) VALUES
+(1, 1, 4, 'Good laptop but a bit expensive.'),
+(2, 2, 5, 'Love this hat!');
+
+INSERT INTO ecommerce_schema.wishlist (user_id, product_id) VALUES
+(1, 2);
+
 INSERT INTO ecommerce_schema.shipment (order_id, tracking_number, carrier, estimated_arrival) VALUES
 (1, 'TRACK12345', 'FedEx', '2023-10-15'),
 (2, 'TRACK67890', 'UPS', '2023-09-25');
