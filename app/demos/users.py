e8d5b6431d62fa52991d0fbe25749fee8db4a79c
esem <stratos.marin@gmail.com>
2023-10-21 11:10:34 +0300

    try to catch duplicates

--- app/demos/users.py
+++ app/demos/users.py
@@ -2,7 +2,7 @@ import logging
 
 from app.backend.session import create_session
 from app.schemas.users import UserCreateSchema
-from app.services.users import UsersService
+from app.services.users import DuplicateEmailError, UsersService
 
 
 def run():
@@ -11,12 +11,15 @@ def run():
     with create_session() as session:
         users_service = UsersService(session)
 
-        # Adding a user
-        new_user_data = UserCreateSchema(
-            username="new_user", password="new_password", email="new.user@example.com", user_type="customer"
-        )
-        added_user = users_service.add_user(new_user_data)
-        logging.info(f"Added User ID: {added_user.user_id} - Username: {added_user.username} - Email: {added_user.email}")
+        try:
+            # Adding a user
+            new_user_data = UserCreateSchema(
+                username="new_user", password="new_password", email="new.user@example.com", user_type="customer"
+            )
+            added_user = users_service.add_user(new_user_data)
+            logging.info(f"Added User ID: {added_user.user_id} - Username: {added_user.username} - Email: {added_user.email}")
+        except DuplicateEmailError:
+            logging.error("Error: Email already exists")
 
         # Retrieve all users
         all_users = users_service.get_all_users()
--- app/services/users.py
+++ app/services/users.py
@@ -1,10 +1,17 @@
 from datetime import date
 
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 
 from app.models.users import Users
 from app.schemas.users import UserCreateSchema, UsersSchema
 from app.services.base import BaseDataManager, BaseService
+
+
+class DuplicateEmailError(Exception):
+    """Raised when trying to add a user with a duplicate email."""
+
+    pass
 
 
 class UsersService(BaseService):
@@ -15,8 +22,11 @@ class UsersService(BaseService):
     def add_user(self, user_data: UserCreateSchema) -> UsersSchema:
         """Add a new user to the database."""
         user = Users(**user_data.dict(), registration_date=date.today())
-        added_user = UsersDataManager(self.session).add_one(user)
-        return UsersSchema.from_orm(added_user)
+        try:
+            added_user = UsersDataManager(self.session).add_one(user)
+            return UsersSchema.from_orm(added_user)
+        except IntegrityError as e:
+            raise DuplicateEmailError("Email already exists") from e
 
 
 class UsersDataManager(BaseDataManager):
