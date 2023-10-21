708bb59224765039158f9af7fad3e2412e5cdf68
esem <stratos.marin@gmail.com>
2023-10-21 07:41:25 +0300

    remove product

--- main.py
+++ main.py
@@ -2,9 +2,7 @@
 # Initializes logging and starts the [entity]demo, e.g. product, customer, etc.
 
 import logging
-from app.demos import product
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting application")
-    product.run()
